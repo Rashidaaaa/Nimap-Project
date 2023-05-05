d7103db31ee35724aa1aeae8cb4bd84f627c3bd6
Rashidaaaa <109268582+Rashidaaaa@users.noreply.github.com>
2023-05-05 18:16:43 +0530

    Add files via upload

---
+++ Home.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Home.aspx.cs" Inherits="Project_for_nimap.Home" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <link rel="stylesheet" type="text/css" href="mycss.css" />
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div class="table-show">
+
+            <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="false" AllowPaging="true" PageSize="10" OnPageIndexChanging="OnPageIndexChanging"  CellSpacing="5" CellPadding="15"
+                >
+                 <Columns>
+                <asp:BoundField DataField="ProductID" HeaderText="ProductID"  HeaderStyle-CssClass="headerTitle" ItemStyle-CssClass="bookTitle titleWidth"/>
+                <asp:BoundField DataField="ProductName" HeaderText="ProductName" HeaderStyle-CssClass="headerTitle" ItemStyle-CssClass="bookTitle titleWidth"/>
+                <asp:BoundField DataField="CategoryID" HeaderText="CategoryID"  HeaderStyle-CssClass="headerTitle" ItemStyle-CssClass="bookTitle titleWidth"/>
+                <asp:BoundField DataField="CategoryName" HeaderText="CategoryName" HeaderStyle-CssClass="headerTitle" ItemStyle-CssClass="bookTitle titleWidth"/>
+            </Columns>
+            </asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Home.aspx.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Project_for_nimap
+{
+    public partial class Home : System.Web.UI.Page
+    {
+        string strcon = ConfigurationManager.ConnectionStrings["myConnectionString"].ConnectionString;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            GridView1.DataSource = getGrid();
+            this.DataBind();
+        }
+
+        public DataSet getGrid()
+        {
+            string queryString = "SELECT Product.ProductID as ProductID, Product.ProductName as ProductName, Category.CategoryID as CategoryID, Category.CategoryName as CategoryName FROM Product JOIN Category ON Product.CategoryID = Category.CategoryID";
+            using (SqlConnection connection = new SqlConnection(strcon))
+            {
+
+                SqlCommand command = new SqlCommand(queryString, connection);
+                SqlDataAdapter adapter = new SqlDataAdapter(command);
+                DataSet dsPubs = new DataSet();
+                try
+                {
+                    connection.Open();
+                    adapter.Fill(dsPubs, "products");
+
+
+
+                }
+                catch (Exception err)
+                {
+                    //error label
+                }
+                finally
+                {
+                    connection.Close();
+                }
+                return dsPubs;
+
+
+
+            }
+
+        }
+        protected void OnPageIndexChanging(object sender, GridViewPageEventArgs e)
+        {
+            GridView1.PageIndex = e.NewPageIndex;
+            GridView1.DataSource = getGrid();
+            this.DataBind();
+        }
+    }
+}
